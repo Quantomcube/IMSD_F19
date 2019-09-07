78de0e71b19298cef0611327577e71cdebe15a56
QuentonBlake <qblake139@gmail.com>
2019-09-07 15:52:13 -0400

    Unity Project

---
+++ New Unity Project/Assets/CubeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public float speed = 1;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Not used
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomInt = Random.Range(-10,11);
+
+        transform.position += new Vector3(0,0,randomInt);
+    }
+
+    
+}
---
+++ New Unity Project/Assets/SphereMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+
+    int randomXPositionThatIsLeftOrRight;
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTheSphereToEitherTheLeftOrRightOfScreen();
+    }
+
+    void MoveTheSphereToEitherTheLeftOrRightOfScreen()
+    {
+        randomXPositionThatIsLeftOrRight = Random.Range(0, 2);
+        transform.position = new Vector3(-10 + 20 * randomXPositionThatIsLeftOrRight, transform.position.y, transform.position.z);
+    }
+
+    
+}
