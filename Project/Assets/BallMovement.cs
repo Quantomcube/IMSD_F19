82b1baab322b5927b2a32746d4cc6eaf89224d95
QuentonBlake <qblake139@gmail.com>
2019-10-02 01:30:24 -0400

    Unity Project

---
+++ Project/Assets/BallMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float ballVelocity = 600f;
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballVelocity, ballVelocity, 0));
+        }
+    }
+}
---
+++ Project/Assets/MovementForPaddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementForPaddle : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+
+    private Vector3 playerPos = new Vector3(0, -16.3f, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -7.7f, 7.7f), -16.3f, 0f);
+        transform.position = playerPos;
+
+    }
+}
